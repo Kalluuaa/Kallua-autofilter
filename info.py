1ca18933738adf40b5db9ee8d8e317f8d711a2bd
Kalluuaa <134672382+Kalluuaa@users.noreply.github.com>
2023-08-27 12:52:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "YourDemandZone")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'YourDemandZone')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'v2.kpslink.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'ca6482dc911d22055cf001d59fc94339eb1b4591')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
@@ -100,5 +100,5 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 #Direct Link Generator By @Developerr_Bots
 DIRECT_GEN_DB = int(environ.get("DIRECT_GEN_DB", "-1001855110365")) # Enter your channel id
-DIRECT_GEN_URL = environ.get("DIRECT_GEN_URL", "https://maxfiletolink.onrender.com/") # https://example.com/
+DIRECT_GEN_URL = environ.get("DIRECT_GEN_URL", "https://maxleechzone-779bc3ae92d2.herokuapp.com/") # https://example.com/
 DIRECT_GEN = bool(DIRECT_GEN_DB and DIRECT_GEN_URL)

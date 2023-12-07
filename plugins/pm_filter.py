73bf7aa7905fd9eebe0c9ebe00adb3ba0d5508aa
Kallluaa <136989645+Kallluaa@users.noreply.github.com>
2023-12-07 16:28:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -11,6 +11,7 @@ lock = asyncio.Lock()
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 import pyrogram
+import humanize
 from helper.progress import get_size, get_time, humanbytes
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive

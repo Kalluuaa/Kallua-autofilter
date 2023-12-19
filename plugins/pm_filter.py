119190ace5419c2a5954099a0b3523bd690d5e87
Kallluaa <136989645+Kallluaa@users.noreply.github.com>
2023-12-19 16:11:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1764,6 +1764,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.SONG_TXT,
+	    disable_web_page_preview=True,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
 	)
